862f7ce292a234cdad107729f3e52598e77b24f9
Stas Averin <averin.developer@gmail.com>
2016-12-26 15:39:35 +0300

    bug in tint on the slider

--- app/src/main/java/com/stasoption/lockunlockslider/LockUnlockSlider.java
+++ app/src/main/java/com/stasoption/lockunlockslider/LockUnlockSlider.java
@@ -16,6 +16,7 @@ import android.support.v4.content.res.ResourcesCompat;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
