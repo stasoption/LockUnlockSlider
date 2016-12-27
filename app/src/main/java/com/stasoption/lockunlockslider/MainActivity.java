56ec45f917c5832bff7e650468f67c28de6e8acd
Stas Averin <averin.developer@gmail.com>
2016-12-27 18:21:53 +0300

    main act

--- app/src/main/java/com/stasoption/lockunlockslider/MainActivity.java
+++ app/src/main/java/com/stasoption/lockunlockslider/MainActivity.java
@@ -33,19 +33,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //here, we can set some parameters (OPTIONAL) for slider
-//        mLockUnlockSlider.setSliderStatus(INIT_STATUS_FOR_SLIDER); //primary bool param (true/false)
-//        mLockUnlockSlider.setThumbHeight(60); //thumb height
-//        mLockUnlockSlider.setThumbWidth(60); //thumb width
-//        mLockUnlockSlider.setGradientForThumb(Color.GRAY, Color.WHITE); //set background for thumb (gradient or monotone)
-//        mLockUnlockSlider.setBackgroundWhenLock(45, Color.parseColor("#929292"), 1, Color.GRAY); // set background for screens when lock or unlock. parameters:
-//        mLockUnlockSlider.setBackgroundWhenUnLock(45, Color.parseColor("#7ed321"), 1, Color.GRAY); // (angle of the shape, color of background, width of border, color of border)
-//        mLockUnlockSlider.setThumbAngle(45); // angle of the thumb
-//        mLockUnlockSlider.setTextWhenLock("Unolck"); // text status of slider when lock
-//        mLockUnlockSlider.setTextWhenUnLock("Lock"); // text status of slider when unlock
-//        mLockUnlockSlider.setTextSize(16); // text size on slider
-//        mLockUnlockSlider.setTextColor(Color.parseColor("#FAF0E6")); //text color on slider
-//        mLockUnlockSlider.setImageThumbWhenLock(ResourcesCompat.getDrawable(getResources(),R.drawable.ic_lock_open_black_24dp, null)); //set icon for the thumb. param:--
-//        mLockUnlockSlider.setImageThumbWhenUnLock(ResourcesCompat.getDrawable(getResources(),R.drawable.ic_lock_outline_black_24dp, null));//--()
+        mLockUnlockSlider.setSliderStatus(INIT_STATUS_FOR_SLIDER); //primary bool param (true/false)
+        mLockUnlockSlider.setThumbHeight(60); //thumb height
+        mLockUnlockSlider.setThumbWidth(60); //thumb width
+        mLockUnlockSlider.setGradientForThumb(Color.GRAY, Color.WHITE); //set background for thumb (gradient or monotone)
+        mLockUnlockSlider.setBackgroundWhenLock(45, Color.parseColor("#929292"), 1, Color.GRAY); // set background for screens when lock or unlock. parameters:
+        mLockUnlockSlider.setBackgroundWhenUnLock(45, Color.parseColor("#7ed321"), 1, Color.GRAY); // (angle of the shape, color of background, width of border, color of border)
+        mLockUnlockSlider.setThumbAngle(45); // angle of the thumb
+        mLockUnlockSlider.setTextWhenLock("Unolck"); // text status of slider when lock
+        mLockUnlockSlider.setTextWhenUnLock("Lock"); // text status of slider when unlock
+        mLockUnlockSlider.setTextSize(16); // text size on slider
+        mLockUnlockSlider.setTextColor(Color.parseColor("#FAF0E6")); //text color on slider
+        mLockUnlockSlider.setImageThumbWhenLock(ResourcesCompat.getDrawable(getResources(),R.drawable.ic_lock_open_black_24dp, null)); //set icon for the thumb. param:--
+        mLockUnlockSlider.setImageThumbWhenUnLock(ResourcesCompat.getDrawable(getResources(),R.drawable.ic_lock_outline_black_24dp, null));//--()
 
         
         //start the slider
